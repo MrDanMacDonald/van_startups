1321407a1777e2fb80ddb4755da6bc7d8623ae7b
MrDanMacDonald <dan@danmacd.com>
2014-12-19 10:25:39 -0800

    successfully create jobs

--- app/services/create_jobs_from_parsed_data.rb
+++ app/services/create_jobs_from_parsed_data.rb
@@ -1,13 +1,11 @@
 class CreateJobsFromParsedData < ActiveRecord::Base
   def self.call(parsed_job_data)
-    # Order of the looping must be messed up, as only 2 jobs are getting created
-    # Rethink how I'm doing this
-    startups = Company.all
     parsed_job_data.each do |job|
-      startups.all.each do |startup|
-        return false if Job.all.map(&:angellist_job_id).include?(job[:angellist_job_id])
-        if job[:angellist_company_id] == startup[:angellist_company_id]
-          startup.jobs.create(job)
+      return false if Job.all.map(&:angellist_job_id).include?(job[:angellist_job_id])
+      Company.all.each do |startup|
+        if startup.angellist_company_id == job[:angellist_company_id]
+          new_job = Job.create(company_id: startup.id)
+          new_job.update_attributes(job)
         end
       end
     end
