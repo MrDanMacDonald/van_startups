683102e71d74eae2a0330deffdcc44582f4e8549
MrDanMacDonald <dan@danmacd.com>
2015-02-14 01:51:47 -0800

    authenticate with token

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141218173730) do
+ActiveRecord::Schema.define(version: 20150214094018) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20141218173730) do
     t.text     "description"
     t.text     "concept"
     t.json     "markets"
+    t.integer  "amount_raised"
   end
 
   add_index "companies", ["angellist_company_id"], name: "index_companies_on_angellist_company_id", using: :btree
--- lib/tasks/update_companies_and_jobs.rake
+++ lib/tasks/update_companies_and_jobs.rake
@@ -1,13 +1,15 @@
 require 'httparty'
-
 namespace :jobs do
 
   # Make API call to AngelList to fetch all jobs in Vancouver
   desc "checks for new companies and updates existing ones"
   task update_jobs: :environment do |args|
 
+    # Access token from Angellist
+    access_token = APP_CONFIG['angellist_access_token']
+
     # Fetches all jobs with the location_tag of 1698 (Vancouver, BC)
-    response = HTTParty.get('https://api.angel.co/1/tags/1698/jobs')
+    response = HTTParty.get("https://api.angel.co/1/tags/1698/jobs?access_token=#{access_token}")
 
     ParseAngellistResponse.call(response)
   end
